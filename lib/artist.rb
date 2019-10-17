2d367058fac3ecd955584091d136d7e4aa7cf412
Kathleen Kamali <kathleenkamali@gmail.com>
2019-10-17 20:14:47 +0000

    Automatically backed up by Learn

--- lib/artist.rb
+++ lib/artist.rb
@@ -5,7 +5,7 @@ require_relative './concerns/paramable.rb'
 class Artist
   attr_accessor :name
   attr_reader :songs
-  extend Memorable 
+  extend Memorable::ClassMethods 
   extend Findable
   include Paramable
   
